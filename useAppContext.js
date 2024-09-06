485d312d050945c46e0eb0db45c902d37cdd0e29
harsheel55 <147084769+harsheel55@users.noreply.github.com>
2024-09-06 21:52:53 +0530

    Delete useAppContext.js

--- useAppContext.js
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-import { AppContext } from "../context/AppContext";
-
-export const useAppContext = () => {
-    const appCtx = useContext(AppContext)
-    return {...appCtx}
-}
